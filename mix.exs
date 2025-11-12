1bcf0848f78d4d20c9271f118785ed042d2ae55c
Sonny Scroggin <sonny@scrogg.in>
2025-11-12 17:17:51 -0600

    Rustler should be optional (#2)
    * Rustler should be optional
    
    * Delete release and recreate after checksum commit

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Wasmex.MixProject do
   use Mix.Project
 
-  @version "0.13.0"
+  @version "0.13.1"
 
   def project do
     [
@@ -28,7 +28,7 @@ defmodule Wasmex.MixProject do
   defp deps do
     [
       {:rustler_precompiled, "~> 0.8"},
-      {:rustler, "~> 0.36.2"},
+      {:rustler, "~> 0.36.2", optional: true},
       {:ex_doc, "~> 0.38.2", only: [:dev, :test]},
       {:credo, "~> 1.7.12", only: [:dev, :test], runtime: false}
     ]
